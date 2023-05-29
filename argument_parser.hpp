02b324cd2a1fb2276f8b47421114a3a6730e91c0
Luca Denti <ldenti92@gmail.com>
2023-05-29 21:32:42 +0200

    Add arg parser

---
+++ argument_parser.hpp
@@ -0,0 +1,106 @@
+#ifndef ARG_PARSER_H_
+#define ARG_PARSER_H_
+
+#include <getopt.h>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+static const char *VERSION = "0.0.1";
+
+// clang-format off
+static const char *USAGE_MESSAGE =
+    "\n"
+    "Usage: sveval <reference.fa> <truth.vcf.gz> <call.vcf.gz>\n"
+    "\n"
+    "      -k, --ksize <int>                 kmers size (default: 13)\n"
+    "      -w, --window <int>                window size (default: 250)\n"
+    "      -t, --trf <trf.bed>               tandem repeats .bed file\n"
+    "      -c, --conf <conf.bed>             confindence regions .bed file\n"
+    "      -r, --region <chr:start-end>      analyze only this region\n"
+    "      -@, --threads <int>               threads (default: 1)\n"
+    "      -v, --version                     print version information\n"
+    "      -h, --help                        display this help and exit\n"
+    "\n";
+// clang-format on
+
+namespace opt {
+static int k = 13;
+static int w = 250;
+static string trf = "";
+static string conf = "";
+static string region = "";
+static int threads = 1;
+static string fa_path;
+static string tvcf_path;
+static string cvcf_path;
+} // namespace opt
+
+static const char *shortopts = "k:w:t:c:r:@:h";
+
+static const struct option longopts[] = {
+    {"ksize", required_argument, NULL, 'k'},
+    {"window", required_argument, NULL, 'w'},
+    {"trf", required_argument, NULL, 't'},
+    {"conf", required_argument, NULL, 'c'},
+    {"region", required_argument, NULL, 'r'},
+    {"threads", required_argument, NULL, '@'},
+    {"version", no_argument, NULL, 'v'},
+    {"help", no_argument, NULL, 'h'},
+    {NULL, 0, NULL, 0}};
+
+void parse_arguments(int argc, char **argv) {
+  bool die = false;
+  for (char c;
+       (c = getopt_long(argc, argv, shortopts, longopts, NULL)) != -1;) {
+    istringstream arg(optarg != NULL ? optarg : "");
+    switch (c) {
+    case 'k':
+      arg >> opt::k;
+      break;
+    case 'w':
+      arg >> opt::w;
+      break;
+    case 't':
+      arg >> opt::trf;
+      break;
+    case 'c':
+      arg >> opt::conf;
+      break;
+    case 'r':
+      arg >> opt::region;
+      break;
+    case '@':
+      arg >> opt::threads;
+      break;
+    case 'v':
+      cerr << "sveval " << VERSION << "\n";
+      exit(EXIT_SUCCESS);
+    case '?':
+      die = true;
+      break;
+    case 'h':
+      cout << USAGE_MESSAGE;
+      exit(EXIT_SUCCESS);
+    }
+  }
+
+  if (argc - optind < 3) {
+    cerr << "sveval : missing arguments\n";
+    die = true;
+  } else if (argc - optind > 3) {
+    cerr << "sveval : too many arguments\n";
+    die = true;
+  }
+  if (die) {
+    cerr << "\n" << USAGE_MESSAGE;
+    exit(EXIT_FAILURE);
+  }
+
+  opt::fa_path = argv[optind++];
+  opt::tvcf_path = argv[optind++];
+  opt::cvcf_path = argv[optind++];
+}
+
+#endif // ARG_PARSER_H_
