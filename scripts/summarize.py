2e7c9a137999cad463b26b98a41328bb500f99fa
Luca Denti <ldenti92@gmail.com>
2023-07-07 13:05:45 +0200

    Update script to plot results

--- scripts/summarize.py
+++ scripts/summarize.py
@@ -9,69 +9,53 @@ import pandas as pd
 
 
 def main():
-    truth_vcf_path = sys.argv[1]
-    call_vcf_path = sys.argv[2]
-    scores_path = sys.argv[3]
-    # tau = float(sys.argv[4])
+    prefix_path = sys.argv[1]
 
     TRUTH = {}
-    TRUTH_L = {}
-    for rec in VariantFile(truth_vcf_path):
-        idx = rec.id
-        TRUTH[idx] = 0
-        TRUTH_L[idx] = (
-            rec.info["SVLEN"]
-            if type(rec.info["SVLEN"]) == int
-            else statistics.fmean(rec.info["SVLEN"])
-        )
     CALL = {}
-    CALL_L = {}
-    for rec in VariantFile(call_vcf_path):
-        idx = rec.id
-        CALL[idx] = 0
-        CALL_L[idx] = (
-            rec.info["SVLEN"]
-            if type(rec.info["SVLEN"]) == int
-            else statistics.fmean(rec.info["SVLEN"])
-        )
+    REGIONS = {}
 
-    for line in open(scores_path):
-        t, idx, score = line.strip("\n").split(" ")
+    TREGIONS = set()
+    CREGIONS = set()
+    for line in open(prefix_path + ".regions.bed"):
+        chrom, s, e, t, l = line.strip("\n").split("\t")
+        region = f"{chrom}:{s}-{e}"
+        if t == "T":
+            TREGIONS.add(region)
+        elif t == "C":
+            CREGIONS.add(region)
+
+    for line in open(prefix_path + ".results.txt"):
+        t, idx, region, score = line.strip("\n").split(" ")
+        score = float(score)
+        if score < 0:
+            score = 0
+            # continue
         if t == "T":
             TRUTH[idx] = float(score)
-        else:
+        elif t == "C":
             CALL[idx] = float(score)
+        else:
+            REGIONS[region] = float(score)
 
     df = []
-    for tau in range(0, 101, 10):
+    for tau in range(0, 101, 1):
         tau = tau / 100
         R = sum([x >= tau for x in TRUTH.values()]) / len(TRUTH)
         P = sum([x >= tau for x in CALL.values()]) / len(CALL)
-        print(sum([x >= tau for x in CALL.values()]), len(CALL))
-        F1 = 2 * P * R / (P + R)
-        df.append([tau, P, R, F1])
-    df = pd.DataFrame(df, columns=["tau", "P", "R", "F1"])
-    print(df)
+        regP = sum([x >= tau for x in REGIONS.values()]) / len(CREGIONS)
+        regR = sum([x >= tau for x in REGIONS.values()]) / len(TREGIONS)
+        df.append([tau, "Correcteness", P])
+        df.append([tau, "Completeness", R])
+        df.append([tau, "Precision (Regions)", regP])
+        df.append([tau, "Recall (Regions)", regR])
+    df = pd.DataFrame(df, columns=["tau", "Metric", "Value"])
+    # print(df)
 
-    # Rdf = pd.DataFrame([[TRUTH[k], TRUTH_L[k]] for k in TRUTH], columns=["Score", "L"])
-    # Pdf = pd.DataFrame([[CALL[k], CALL_L[k]] for k in CALL], columns=["Score", "L"])
+    sns.lineplot(df, x="tau", y="Value", hue="Metric")
 
-    # fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
-    # sns.histplot(
-    #     data=Rdf, x="Score", ax=ax1, element="step", fill=False, cumulative=True
-    # )
-    # sns.histplot(
-    #     data=Pdf, x="Score", ax=ax2, element="step", fill=False, cumulative=True
-    # )
-
-    # sns.histplot(Rdf, x="Score", y="L", ax=ax3)
-    # sns.histplot(Pdf, x="Score", y="L", ax=ax4)
-
-    # ax1.set_title("R")
-    # ax2.set_title("P")
-    # plt.tight_layout()
-    # # plt.savefig("plot.png")
-    # plt.savefig(scores_path + ".png")
+    plt.tight_layout()
+    plt.savefig("plot.png")
 
 
 if __name__ == "__main__":
