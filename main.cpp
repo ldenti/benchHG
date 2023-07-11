d8501a4635e223d6b78acdc70873ebb22c23d2cd
Luca Denti <ldenti92@gmail.com>
2023-07-11 18:11:01 +0200

    Major update - reimplement subpath check for reference path - first comparison round over regions based on SV type - simplify output (no need to reiterate over VCFs)

--- main.cpp
+++ main.cpp
@@ -24,38 +24,15 @@
 
 using namespace std;
 
-string seq_align(const char *tseq, const char *qseq, int sc_mch, int sc_mis,
-                 int gapo, int gape) {
-  int i, a = sc_mch, b = sc_mis < 0 ? sc_mis : -sc_mis; // a>0 and b<0
-  int8_t mat[25] = {a, b, b, b, 0, b, a, b, b, 0, b, b, a,
-                    b, 0, b, b, b, a, 0, 0, 0, 0, 0, 0};
-  int tl = strlen(tseq), ql = strlen(qseq);
-  uint8_t *ts, *qs, c[256];
-  ksw_extz_t ez;
-
-  memset(&ez, 0, sizeof(ksw_extz_t));
-  memset(c, 4, 256);
-  c['A'] = c['a'] = 0;
-  c['C'] = c['c'] = 1;
-  c['G'] = c['g'] = 2;
-  c['T'] = c['t'] = 3; // build the encoding table
-  ts = (uint8_t *)malloc(tl);
-  qs = (uint8_t *)malloc(ql);
-  for (i = 0; i < tl; ++i)
-    ts[i] = c[(uint8_t)tseq[i]]; // encode to 0/1/2/3
-  for (i = 0; i < ql; ++i)
-    qs[i] = c[(uint8_t)qseq[i]];
-  ksw_extz(0, ql, qs, tl, ts, 5, mat, gapo, gape, -1, -1, 0, &ez);
-  string cigar = "";
-  for (i = 0; i < ez.n_cigar; ++i) {
-    cigar += to_string(ez.cigar[i] >> 4) + "MID"[ez.cigar[i] & 0xf];
-    // printf("%d%c", ez.cigar[i]>>4, "MID"[ez.cigar[i]&0xf]);
-  }
-  // putchar('\n');
-  free(ez.cigar);
-  free(ts);
-  free(qs);
-  return cigar;
+bool is_subpath(const vector<int> &A, const vector<int> &B) {
+  // FIXME: is this the best way to do this?
+  string sA = "";
+  for (const int &a : A)
+    sA += to_string(a);
+  string sB = "";
+  for (const int &b : B)
+    sB += to_string(b);
+  return sA.find(sB) != sA.npos;
 }
 
 int main(int argc, char *argv[]) {
@@ -70,91 +47,135 @@ int main(int argc, char *argv[]) {
   string filtered_cvcf_path;
   vector<string> regions;
   ofstream regions_file(opt::out + ".regions.bed");
-  if (opt::region.compare("") == 0) {
-    spdlog::info("Creating trees..");
-    Locator l(opt::k, opt::w, opt::W);
-    l.add_conf(opt::conf);
-    l.add_trf(opt::trf);
-    spdlog::info("Locating regions of interest from truth..");
-    filtered_tvcf_path = opt::out + ".truth.vcf.gz";
-    l.parse_truth(fai, opt::tvcf_path, filtered_tvcf_path);
-    spdlog::info("Locating regions of interest from call..");
-    filtered_cvcf_path = opt::out + ".predictions.vcf.gz";
-    l.parse_call(fai, opt::cvcf_path, filtered_cvcf_path);
-    spdlog::info("Intersecting regions..");
-    l.intersect(regions_file);
-    regions = l.get_regions(regions_file);
-  } else {
-    regions.push_back(opt::region);
-    filtered_tvcf_path = opt::tvcf_path;
-    filtered_cvcf_path = opt::cvcf_path;
-  }
+  spdlog::info("Creating trees..");
+  Locator l(opt::k, opt::w, opt::W);
+  l.add_conf(opt::conf);
+  l.add_trf(opt::trf);
+  spdlog::info("Locating regions of interest from truth..");
+  filtered_tvcf_path = opt::out + ".truth.vcf.gz";
+  l.parse_truth(fai, opt::tvcf_path, filtered_tvcf_path);
+  spdlog::info("Locating regions of interest from call..");
+  filtered_cvcf_path = opt::out + ".predictions.vcf.gz";
+  l.parse_call(fai, opt::cvcf_path, filtered_cvcf_path);
+  spdlog::info("Intersecting regions..");
+  l.intersect(regions_file);
+  regions = l.get_regions(regions_file);
+
   fai_destroy(fai);
   regions_file.close();
-
   if (opt::regions_only)
     return 0;
 
-  spdlog::info("Preallocating data..");
+  spdlog::info("Cleaning regions..");
+
   map<string, pair<string, float>> truth_results;
   map<string, pair<string, float>> call_results;
   map<string, float> regions_results;
+  vector<string> clean_regions;
+
+  bcf_srs_t *sr = bcf_sr_init();
+  bcf_sr_set_opt(sr, BCF_SR_PAIR_LOGIC, BCF_SR_PAIR_BOTH_REF);
+  bcf_sr_set_opt(sr, BCF_SR_REQUIRE_IDX);
+  bcf_sr_add_reader(sr, filtered_tvcf_path.c_str());
+  bcf_sr_add_reader(sr, filtered_cvcf_path.c_str());
+  bcf_hdr_t *thdr = bcf_sr_get_header(sr, 0);
+  bcf_hdr_t *chdr = bcf_sr_get_header(sr, 1);
+
+  filtered_cvcf_path = opt::out + ".predictions.clean.vcf.gz";
+
+  htsFile *cvcf_o = bcf_open(filtered_cvcf_path.c_str(), "wz");
+  bcf_hdr_write(cvcf_o, chdr);
+
+  bcf1_t *trec, *crec;
+  int ndst = 8;
+  char *svtype = (char *)malloc(ndst);
+
+  for (const string &region : regions) {
+    regions_results[region] = -1.0;
+
+    string seq_name;
+    int64_t start_pos = -1, stop_pos = -1;
+    vg::parse_region(region, seq_name, start_pos, stop_pos);
+
+    int true_vtypes = 0;
+    bool tend = false, cend = false;
+    bool is_clean_region = false;
+
+    bcf_sr_seek(sr, seq_name.c_str(), start_pos);
+    while (bcf_sr_next_line(sr)) {
+      if (tend && cend)
+        break;
+      // truth
+      if (bcf_sr_has_line(sr, 0)) {
+        trec = bcf_sr_get_line(sr, 0);
+        if (strcmp(seq_name.c_str(), bcf_hdr_id2name(thdr, trec->rid)) == 0 &&
+            trec->pos <= stop_pos) {
+          // FIXME: assuming SVTYPE to be in the INFO
+          // svtype_info = bcf_get_info(thdr, trec, "SVTYPE");
+          // svtype_idx = svtype_info->key;
+          // const char *key = thdr->id[BCF_DT_ID][svtype_idx].key;
+          bcf_get_info_string(thdr, trec, "SVTYPE", &svtype, &ndst);
+          true_vtypes |= strcmp(svtype, "INS") == 0 ? 1 : 2;
+          truth_results[trec->d.id] = make_pair(region, -1.0);
+        } else
+          tend = true;
+      }
+      // predictions
+      if (bcf_sr_has_line(sr, 1)) {
+        crec = bcf_sr_get_line(sr, 1);
+        if (strcmp(seq_name.c_str(), bcf_hdr_id2name(chdr, crec->rid)) == 0 &&
+            crec->pos <= stop_pos) {
+          bcf_get_info_string(chdr, crec, "SVTYPE", &svtype, &ndst);
+          if (((strcmp(svtype, "INS") == 0 ? 1 : 2) & true_vtypes)) {
+            bcf_write1(cvcf_o, chdr, crec);
+            is_clean_region = true;
+          }
+          call_results[crec->d.id] = make_pair(region, -1.0);
+        } else
+          cend = true;
+      }
+    }
+    if (is_clean_region)
+      // push the region if we have a type match
+      clean_regions.push_back(region);
+  }
+  free(svtype);
+  bcf_sr_destroy(sr);
+  bcf_close(cvcf_o);
+  tbx_index_build2(filtered_cvcf_path.c_str(),
+                   (filtered_cvcf_path + ".tbi").c_str(), 14, &tbx_conf_vcf);
+
+  spdlog::info("Preallocating data..");
   vector<faidx_t *> FAI(opt::threads);
   vector<char *> REGION(opt::threads);
   // vector<char *> REGION_SEQ(opt::threads);
-
-  for (uint i = 0; i < regions.size(); ++i) {
-    regions_results[regions[i]] = -1.0;
-  }
-
-  htsFile *vcf = bcf_open(filtered_tvcf_path.c_str(), "r");
-  bcf_hdr_t *vcf_header = bcf_hdr_read(vcf);
-  bcf1_t *vcf_record = bcf_init();
-  while (bcf_read(vcf, vcf_header, vcf_record) == 0) {
-    bcf_unpack(vcf_record, BCF_UN_STR);
-    truth_results[vcf_record->d.id] = make_pair("", -1.0);
-  }
-  bcf_hdr_destroy(vcf_header);
-  bcf_close(vcf);
-
-  vcf = bcf_open(filtered_cvcf_path.c_str(), "r");
-  vcf_header = bcf_hdr_read(vcf);
-  vcf_record = bcf_init();
-  while (bcf_read(vcf, vcf_header, vcf_record) == 0) {
-    bcf_unpack(vcf_record, BCF_UN_STR);
-    call_results[vcf_record->d.id] = make_pair("", -1.0);
-  }
-  bcf_hdr_destroy(vcf_header);
-  bcf_close(vcf);
 
   for (uint i = 0; i < opt::threads; ++i) {
     FAI[i] = fai_load3_format(opt::fa_path.c_str(), NULL, NULL, FAI_CREATE,
                               FAI_FASTA);
     REGION[i] = (char *)malloc(512);
   }
+
   char *tvcf_path = (char *)malloc(filtered_tvcf_path.size() + 1);
   strcpy(tvcf_path, filtered_tvcf_path.c_str());
   tvcf_path[filtered_tvcf_path.size()] = '\0';
 
-  spdlog::info("Starting analysis of {} regions..", regions.size());
+  spdlog::info("Starting analysis of {} regions..", clean_regions.size());
   omp_set_dynamic(0);
   omp_set_num_threads(opt::threads);
 
 #pragma omp parallel for
-  for (uint i = 0; i < regions.size(); ++i) {
-    strcpy(REGION[omp_get_thread_num()], regions[i].c_str());
-    REGION[omp_get_thread_num()][regions[i].size()] = '\0';
+  for (uint i = 0; i < clean_regions.size(); ++i) {
+    strcpy(REGION[omp_get_thread_num()], clean_regions[i].c_str());
+    REGION[omp_get_thread_num()][clean_regions[i].size()] = '\0';
 
     string seq_name;
     int64_t start_pos = -1, stop_pos = -1;
     vg::parse_region(REGION[omp_get_thread_num()], seq_name, start_pos,
                      stop_pos);
 
-    // if (stop_pos - start_pos + 1 > 75000)
-    //   continue;
-
 #ifdef PDEBUG
-    cerr << omp_get_thread_num() << " " << regions[i] << " ("
+    cerr << omp_get_thread_num() << " " << clean_regions[i] << " ("
          << stop_pos - start_pos + 1 << ")" << endl;
 #endif
 
@@ -172,11 +193,12 @@ int main(int argc, char *argv[]) {
     c2.destroy_data();
 
     if (hap1 == "" or hap2 == "") {
-      spdlog::warn("Skipping {} due to error in consensus", regions[i]);
+      spdlog::warn("Skipping {} due to error in consensus", clean_regions[i]);
       continue;
     }
     if (!c1.has_alts && !c2.has_alts) {
-      spdlog::warn("Skipping {} since no alt alleles in truth", regions[i]);
+      spdlog::warn("Skipping {} since no alt alleles in truth",
+                   clean_regions[i]);
       continue;
     }
 
@@ -192,13 +214,13 @@ int main(int argc, char *argv[]) {
     graph.build();
     graph.analyze();
 #ifdef PDEBUG
-    // graph.to_gfa();
+    graph.to_gfa();
     for (const int &n : graph.ref_path)
       cerr << n << " ";
     cerr << endl;
 #endif
 
-    float gscore1 = -1, gscore2 = -1, rscore1 = -1, rscore2 = -1;
+    float gscore1 = -1, gscore2 = -1;
     vector<string> haps;
     if (c1.has_alts)
       haps.push_back(hap1);
@@ -225,38 +247,6 @@ int main(int argc, char *argv[]) {
       path1 = path2;
     }
 
-    //     Alignment refal1, refal2;
-    //     if (c1.has_alts) {
-    //       string cigar = seq_align(region_seq, hap1.c_str(), 1, -2, 2, 1);
-    //       refal1 = {-1, hap1.size(), 0, cigar, vector<int>(), 0.0};
-    //       refal1.set_score();
-    //       rscore1 = refal1.score;
-    // #ifdef PDEBUG
-    //       cerr << hap1.size() << " " << refal1.cigar << " " << rscore1 <<
-    //       endl;
-    // #endif
-    //     } else {
-    // #ifdef PDEBUG
-    //       cerr << "No ref alignment 1" << endl;
-    // #endif
-    //     }
-    //     if (c2.has_alts) {
-    //       string cigar = seq_align(region_seq, hap2.c_str(), 1, -2, 2, 1);
-    //       refal2 = {-1, hap2.size(), 0, cigar, vector<int>(), 0.0};
-    //       refal2.set_score();
-    //       rscore2 = refal2.score;
-    // #ifdef PDEBUG
-    //       cerr << hap2.size() << " " << refal2.cigar << " " << rscore2 <<
-    //       endl;
-    // #endif
-    //     } else {
-    // #ifdef PDEBUG
-    //       cerr << "No ref alignment 2" << endl;
-    // #endif
-    //     }
-    //     rscore1 = rscore1 == -1 ? rscore2 : rscore1;
-    //     rscore2 = rscore2 == -1 ? rscore1 : rscore2;
-
 #ifdef PDEBUG
     for (const auto &a : al.alignments) {
       cerr << a.id << " " << a.cigar << " ";
@@ -266,16 +256,10 @@ int main(int argc, char *argv[]) {
     }
 #endif
 
-    // assert(rscore1 != 1 && rscore2 != 1);
-    assert(gscore1 != -1 &&
-           gscore2 != -1 /**&& rscore1 != -1 && rscore2 != -1**/);
+    assert(gscore1 != -1 && gscore2 != -1);
 
-    float score1 = !equal(path1.begin(), path1.end(), graph.ref_path.begin())
-                       ? gscore1
-                       : 0;
-    float score2 = !equal(path2.begin(), path2.end(), graph.ref_path.begin())
-                       ? gscore2
-                       : 0;
+    float score1 = !is_subpath(graph.ref_path, path1) ? gscore1 : 0;
+    float score2 = !is_subpath(graph.ref_path, path2) ? gscore2 : 0;
     float score = (score1 + score2) / 2.0;
 
 #ifdef PDEBUG
@@ -290,10 +274,10 @@ int main(int argc, char *argv[]) {
     cs.compute();
 
     for (const pair<string, float> res : ts.results)
-      truth_results.at(res.first) = make_pair(regions[i], res.second);
+      truth_results.at(res.first) = make_pair(clean_regions[i], res.second);
     for (const pair<string, float> res : cs.results)
-      call_results.at(res.first) = make_pair(regions[i], res.second);
-    regions_results.at(regions[i]) = score;
+      call_results.at(res.first) = make_pair(clean_regions[i], res.second);
+    regions_results.at(clean_regions[i]) = score;
 
     free(region_seq);
   }
@@ -312,33 +296,13 @@ int main(int argc, char *argv[]) {
           << " " << region.first << " " << region.second << endl;
 
   spdlog::info("Outputting truth scores..");
-  vcf = bcf_open(filtered_tvcf_path.c_str(), "r");
-  vcf_header = bcf_hdr_read(vcf);
-  vcf_record = bcf_init();
-  string idx;
-  while (bcf_read(vcf, vcf_header, vcf_record) == 0) {
-    bcf_unpack(vcf_record, BCF_UN_STR);
-    idx = vcf_record->d.id;
-    ofile << "T " << idx << " " << truth_results[idx].first << " "
-          << truth_results[idx].second << endl;
-  }
-  bcf_hdr_destroy(vcf_header);
-  bcf_close(vcf);
-
+  for (const auto &r : truth_results)
+    ofile << "T " << r.first << " " << r.second.first << " " << r.second.second
+          << endl;
   spdlog::info("Outputting call scores..");
-  vcf = bcf_open(filtered_cvcf_path.c_str(), "r");
-  vcf_header = bcf_hdr_read(vcf);
-  vcf_record = bcf_init();
-  while (bcf_read(vcf, vcf_header, vcf_record) == 0) {
-    bcf_unpack(vcf_record, BCF_UN_STR);
-    idx = vcf_record->d.id;
-    ofile << "C " << idx << " " << call_results[idx].first << " "
-          << call_results[idx].second << endl;
-  }
-  ofile.close();
-  bcf_hdr_destroy(vcf_header);
-  bcf_close(vcf);
-  bcf_destroy(vcf_record);
+  for (const auto &r : call_results)
+    ofile << "C " << r.first << " " << r.second.first << " " << r.second.second
+          << endl;
 
   return 0;
 }
